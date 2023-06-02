9d5c454c47603a9b04b526d120916c78c2a60443
Austin Wolff <austin.e.wolff@gmail.com>
2023-06-02 10:14:08 -0500

    Add styling to highlight sqs as user mouseOver

--- main.js
+++ main.js
@@ -1,52 +1,36 @@
-function createDivs(count) {
-    let divArray = [];
-    for (let i = 0; i < count; i++) {
-        let loopDiv = document.createElement('div');
-        let innerDiv = document.createElement('div');
-        innerDiv.classList.add('inner-box');
-        loopDiv.classList.add('box');
-        innerDiv.innerText = '.';
-        innerDiv.addEventListener('mouseover', onHandleOnMouseOver);
-        innerDiv.addEventListener("mouseout", onHandleOnMouseOut)
-        loopDiv.appendChild(innerDiv);
-        divArray.push(loopDiv);
-        console.log(`loopDiv = ${loopDiv}`);
-
-    }
-    return divArray;
-}
-function onHandleOnMouseOver(e) {
-
-    let r = randomBetween(0, 255);
-    let g = randomBetween(0, 255);
-    let b = randomBetween(0, 255);
-    let rgb = `rgb(${r},${g},${b})`;
-    e.target.style['background-color'] = rgb;
-}
-function onHandleOnMouseOut(e) {
-    e.target.style['background-color'] = 'grey';
-}
-function onClick(e) {
-    let dimension = prompt("How many squares per side?", 16);
-    console.log(`dimension ${dimension}`);
-    let container = document.querySelector('.container');
-    let boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => container.removeChild(box));
-    let array = createDivs(dimension * dimension);
-    let basis = (100 / dimension).toString() + '%';
-    console.log(`basis ${basis}`);
-
-    array.forEach(div => div.style['flex-basis'] = basis);
-
-    array.forEach(div => container.appendChild(div));
-}
-
-let array = createDivs(16 * 16);
 let container = document.querySelector('.container');
-console.log(`container = ${container}`);
-array.forEach(div => container.appendChild(div));
+let sqrBtn = document.querySelector('#squareBtn');
+sqrBtn.addEventListener('click', onClick);
+console.log(container);
+
+let numberSquaresPerSide = 4;
+let totalSquares = numberSquaresPerSide*numberSquaresPerSide;
+drawSquares(totalSquares);
 
 
-function randomBetween(min, max) {
-    return min + Math.floor(Math.random() * (max - min + 1));
-};
+function drawSquares(totalSquares){
+    container.innerHTML = '';    
+
+
+    for(let i = 0; i<totalSquares; i++){
+        let div = document.createElement("div");
+        div.classList.add("square");
+
+        let length = (100)/numberSquaresPerSide;
+        console.log(length);
+        length = length.toString() +'%';
+        div.style.width = length;
+        div.style.height = length;
+
+
+        container.appendChild(div);
+    
+    }
+}
+
+
+function onClick(e){
+    numberSquaresPerSide = prompt('How many squares per side?');
+    totalSquares = numberSquaresPerSide * numberSquaresPerSide;
+    drawSquares(totalSquares);
+}
